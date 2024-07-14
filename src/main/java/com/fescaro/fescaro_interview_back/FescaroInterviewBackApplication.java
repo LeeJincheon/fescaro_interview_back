46037f95fbc5c9207865c1316f5e1a1eed38a3d2
pressky <pressky99@gmail.com>
2024-07-14 12:58:47 +0900

    init: 의존성 설치 및 yml 설정

---
+++ src/main/java/com/fescaro/fescaro_interview_back/FescaroInterviewBackApplication.java
@@ -0,0 +1,13 @@
+package com.fescaro.fescaro_interview_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FescaroInterviewBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FescaroInterviewBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fescaro/fescaro_interview_back/FescaroInterviewBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fescaro.fescaro_interview_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FescaroInterviewBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
